d70e4aa02e9512fbc61b4dda53eb429db6291f61
Enoch Kim <jihok0519@gmail.com>
2022-05-30 13:52:46 -0400

    WIP: set up dependencies

---
+++ controller/playlist.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const Playlist = require('../models/playlist');
+
+module.exports = router;
---
+++ controller/song.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const Song = require('../models/song');
+
+module.exports = router;
